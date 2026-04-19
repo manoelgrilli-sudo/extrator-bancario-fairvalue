152fe5a39cd4946cb702fa754f754c31366e1926
manoelgrilli-sudo <manoelgrilli@gmail.com>
2026-04-19 10:06:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 
 st.set_page_config(page_title="Fair Value - Extrator Bancário", layout="wide")
 
-st.title("🏦 Extrator Fair Value: Extração Integral")
+st.title("🏦 Fair Value: Extração Bancário")
 
 arquivo_pdf = st.file_uploader("Selecione o Extrato Bancário (PDF)", type="pdf")
 
